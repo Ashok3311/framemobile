34f83f0f9b049b897a0a490cf93ef20f6897e788
Ashok3311 <ashokupreti8911@gmail.com>
2025-03-09 09:45:53 +0545

    changes in cmt

--- framemobile/lib/main.dart
+++ framemobile/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:framemobile/pages/buttomnav.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'firebase_options.dart'; // Import the generated fil
+import 'firebase_options.dart'; // Import the generated file
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
